e991d825e8ccc8ba883eb0ba2d5791f930c52320
AbolfazlAdhami <aadhami880@gmail.com>
2025-07-01 01:44:09 +0330

    feat(middlware): read and access to sesstion SQLite

--- app.js
+++ app.js
@@ -1,69 +1,71 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const csrf = require('csurf');
-const passport = require('passport');
-const logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const csrf = require("csurf");
+const passport = require("passport");
+const logger = require("morgan");
 
 // pass the session to the connect sqlite3 module
 // allowing it to inherit from session.Store
-const SQLiteStore = require('connect-sqlite3')(session);
+const SQLiteStore = require("connect-sqlite3")(session);
 
-const indexRouter = require('./routes/index');
-const authRouter = require('./routes/auth');
+const indexRouter = require("./routes/index");
+const authRouter = require("./routes/auth");
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.locals.pluralize = require('pluralize');
+app.locals.pluralize = require("pluralize");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false, // don't save session if unmodified
-  saveUninitialized: false, // don't create session until something stored
-  store: new SQLiteStore({ db: 'sessions.db', dir: './const/db' })
-}));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: new SQLiteStore({ db: "sessions.db", dir: "./var/db" }),
+  })
+);
+app.use(passport.authenticate("session"));
 app.use(csrf());
-app.use(passport.authenticate('session'));
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   const msgs = req.session.messages || [];
   res.locals.messages = msgs;
-  res.locals.hasMessages = !! msgs.length;
+  res.locals.hasMessages = !!msgs.length;
   req.session.messages = [];
   next();
 });
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.locals.csrfToken = req.csrfToken();
   next();
 });
 
-app.use('/', indexRouter);
-app.use('/', authRouter);
+app.use("/", indexRouter);
+app.use("/", authRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
